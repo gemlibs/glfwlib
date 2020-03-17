10ee4083c6bd901fe07593a21589e15ca15dba5d
ro <ro@ro>
2020-03-16 20:16:13 -0400

    glm rotate

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+using namespace std;
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp> // + glm::perspective
+using namespace glm;
+
 int main(void) {
   if (!glfwInit()) return -1;
   GLFWwindow* window = glfwCreateWindow(640, 480, "Hello", NULL, NULL);
@@ -7,15 +14,21 @@ int main(void) {
     glfwTerminate();
     return -1;
   }
-
   glfwMakeContextCurrent(window);
   glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-  while (!glfwWindowShouldClose(window)) {
-    glClear(GL_COLOR_BUFFER_BIT);
-    // ...
-    glfwSwapBuffers(window);
-    glfwPollEvents();
+
+  glm::mat4 tr = glm::rotate(glm::mat4(1.f), glm::radians(30.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+  glm::vec4 vector(0.f, 5.f, 0.f, 0.f);
+  glm::vec4 tv = tr * vector;
+
+  for(int i = 0; i < 4; i++) {
+    cout << tv[i] << ' ';
   }
+  cout << endl;
+
   glfwTerminate();
   return 0;
 }
+
+
+
